7c803bc410faf1dc5ac7cb3a7ba8fb7aff41d352
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 20:54:16 +0900

    EP202
    変数の型の誤り

---
+++ EP202/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EP202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hiro302";
+            Console.WriteLine("My name is {0}", name);
+        }
+    }
+}
