08a7300ac323819365ac7c97a20a18940df8726e
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 19:44:21 +0900

    キャスト
    明示的な型変換

---
+++ S_204/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace S_204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, d;
+            int c;
+            a = 1.23;
+            // キャストありの代入
+            b = (double)12;
+            c = (int)1.23;
+            // キャストなしの代入
+            d = 10;
+
+            // 表示
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+        }
+    }
+}
