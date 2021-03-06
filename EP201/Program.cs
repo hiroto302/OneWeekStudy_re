63abbf7c365bb125140432d40ac96e901083996f
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 20:44:14 +0900

    EP201
    2つの値のコンソールからの入力されたもの、計算するコード

--- EP201/Program.cs
+++ EP201/Program.cs
@@ -6,7 +6,21 @@ namespace EP201
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 変数の宣言
+            int a,b;
+            // コンソールからの入力 a
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            // コンソールからの入力 b
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+
+            // 計算式の表示
+            Console.WriteLine(" a + b = {0}", a + b);
+            Console.WriteLine(" a - b = {0}", a - b);
+            Console.WriteLine(" a * b = {0}", a * b);
+            Console.WriteLine(" a / b = {0}", a / b);
+            Console.WriteLine(" a % b = {0}", a % b);
         }
     }
 }
