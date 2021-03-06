1c9f81712a90b28f5667190a8931c6d95d00d50a
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 20:02:25 +0900

    コンソールからの入力

---
+++ S_205/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace S_205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+            // 1つ目の文字列入力
+            Console.Write("str1 = ");
+            str1 = Console.ReadLine();
+            // 2つ目の文字列入力
+            Console.Write("str2 = ");
+            str2 = Console.ReadLine();
+            // 表示
+            Console.WriteLine("str1 + str2 = {0}", str1 + str2);
+        }
+    }
+}
