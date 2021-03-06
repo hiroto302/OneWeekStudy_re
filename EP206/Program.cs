e28a060a1a20e2fa633cdd6774c31f2aacd4191b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 23:55:53 +0900

    条件分岐

---
+++ EP206/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EP206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 温度の変数
+            int temperature;
+            // 入力
+            Console.Write("H2Oの温度 : ");
+            temperature = int.Parse(Console.ReadLine());
+            // 判定
+            if(100 <= temperature)
+            {
+                Console.WriteLine("気体");
+            }
+            else if( 0 >= temperature)
+            {
+                Console.WriteLine("固体");
+            }
+            else
+            {
+                Console.WriteLine("液体");
+            }
+        }
+    }
+}
