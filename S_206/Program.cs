83aa278a2ba356ab55804e393454385e399a830c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 20:18:39 +0900

    const キーワード

---
+++ S_206/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace S_206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER = 100;
+            const string STRING = "Hoge";
+
+            // const キーワード : 他のものを代入を防ぐ役割
+            // NUMBER = 90;
+            // STRING = "Fuga";
+
+            Console.WriteLine(NUMBER);
+            Console.WriteLine(STRING);
+
+        }
+    }
+}
