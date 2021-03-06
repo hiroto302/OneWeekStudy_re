148e9eb114733182f9bde1dd04d156dd933198e0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-06 21:50:22 +0900

    条件分岐 if, 比較演算子

---
+++ S_210/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace S_210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("サイコロの目（１〜６）を入力 : ");
+            // サイコロの目を入力
+            int dice = int.Parse(Console.ReadLine());
+            // サイコロの目が範囲内であるか
+            if ( 1 <= dice && dice <= 6)
+            {
+                // サイコロの目が偶数の時
+                if (dice % 2 == 0)
+                {
+                    Console.WriteLine("チョウです");
+                }
+                // 奇数の時
+                else if( dice % 2 == 1)
+                {
+                    Console.WriteLine("ハンです");
+                }
+            }
+            else
+            {
+                Console.WriteLine("範囲内の値ではありません");
+            }
+        }
+    }
+}
