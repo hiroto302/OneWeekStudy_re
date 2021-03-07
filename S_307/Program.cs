7843a201ed8e9f842e3515198a28f5de77b5f575
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 14:37:05 +0900

    配列の宣言と初期化

---
+++ S_307/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace S_307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = {5, 4, 3, 2, 1 };
+            string[] s = {"a", "b", "c"};
+            int i;
+            // 整数型配列の要素表示
+            for(i = 0; i < n.Length; i++)
+            {
+                Console.Write(n[i] + " ");
+            }
+            Console.WriteLine();
+            // 文字列型配列の要素表示
+            for(i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
