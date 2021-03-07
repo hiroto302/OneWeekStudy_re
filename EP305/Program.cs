49a7dae1785e72386f9fb9e7e4581b586a423dfd
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 13:18:09 +0900

    for文,九九表, 桁揃え{0, 0:d}

---
+++ EP305/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EP305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.Write("{0}*{1}={2, 2:d} ", i, j, i * j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
