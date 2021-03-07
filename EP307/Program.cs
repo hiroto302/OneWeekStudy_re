341c21abc86ae0217af35f65f050732f9822db8d
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 20:26:39 +0900

    奇数と偶数の表示

---
+++ EP307/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EP307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Randomクラスのインスタンス生成
+            Random rnd = new Random();
+            // 整数の配列
+            int[] n = new int[10];
+            // 1~100乱数10個生成
+            for(int i = 0; i < n.Length; i++)
+            {
+                n[i] = rnd.Next(1, 101);
+                Console.Write(n[i] + " ");
+            }
+            Console.WriteLine();
+            // 偶数と奇数の判定と表示
+            string[] names = {"偶数 : ", "奇数 : "};
+            for(int i = 0;  i < names.Length; i++)
+            {
+                Console.Write(names[i]);
+                foreach( int j in n)
+                {
+                    if(j % 2 == i)
+                    {
+                        Console.Write(j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
