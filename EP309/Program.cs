c924bd0db8847532aece97ef1c83afb9920e8b66
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 21:43:10 +0900

    配列

---
+++ EP309/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EP309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1~99の乱数を10個生成
+            Random rnd = new Random();
+            int[] n = new int[10];
+            for(int i = 0; i < n.Length; i++)
+            {
+                n[i] = rnd.Next(1, 100);
+                //表示
+                Console.Write(n[i] + " ");
+            }
+            Console.WriteLine();
+
+            // 0以上10未満のｎ配列の要素を表示
+            for(int i = 0; i < 100; i += 10)
+            {
+                Console.Write("{0}以上{1}未満 : ", i , i + 10);
+                foreach( int j in n)
+                {
+                    if(i <= j && j < i +10)
+                    {
+                        Console.Write(j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
