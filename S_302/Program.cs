e10a48e36462a1ab90616ef38cbdd5563661097a
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 10:26:35 +0900

    for文, 多重ループ

---
+++ S_302/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace S_302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 2; i++)
+            {
+                for(int j = 1; j <= 3; j++)
+                {
+                    Console.Write("{0}+{1}={2} ", i, j, i + j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
