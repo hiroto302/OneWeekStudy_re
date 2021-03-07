1217f8db93331dde01c0f35fd830299e825859aa
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 14:53:23 +0900

    foreach( var item in collection)

---
+++ S_308/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace S_308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 配列の宣言と初期化
+            int[] n = {1, 2, 3, 4, 5};
+            // 要素の表示
+            foreach (var i in n)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
