fd3e923fab7ee411fc3a72f8cfadc27325a71c81
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-07 12:27:57 +0900

    for文,return
    while , breakでも記述できる

--- EP302/Program.cs
+++ EP302/Program.cs
@@ -6,7 +6,29 @@ namespace EP302
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 入力される値
+            int n;
+            // 入力
+            Console.Write("正数を入力 : ");
+            n = int.Parse(Console.ReadLine());
+            // 正数であるか
+            if( 0 < n )
+            {
+                // 約数を割出し、表示
+                for(int i = n; 0 < i; i--)
+                {
+                    // 約数の判定
+                    if(n % i == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("正数を入力せよ");
+            }
         }
     }
 }
---
+++ EP303/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EP303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ランダムに生成される数
+            int rndNumber;
+            // 入力される数
+            int inputNumber;
+            // ランダムクラスのインスタンス生成
+            Random rnd = new Random();
+            // 乱数生成 (0~10)
+            // rndNumber = rnd.Next(0,11);
+            rndNumber = rnd.Next(11);
+
+            // ゲーム開始
+            Console.WriteLine("数当てゲーム");
+            Console.WriteLine("0~10の値を入力して下さい");
+            // ３回以内に当てる
+            for(int i = 1; i <= 3; i++)
+            {
+                Console.Write(i + "回目 : ");
+                inputNumber = int.Parse(Console.ReadLine());
+                // 正解の場合
+                if(inputNumber == rndNumber)
+                {
+                    Console.WriteLine("正解です");
+                    return;
+                }
+                // 数が大きい時
+                else if( inputNumber > rndNumber)
+                {
+                    Console.WriteLine("大きい数です");
+                }
+                // 小さい時
+                else
+                {
+                    Console.WriteLine("小さい数です");
+                }
+            }
+            // 3回以内に当てれない時
+            Console.WriteLine("ゲームオーバー");
+            Console.WriteLine("正解は," + rndNumber + "です！");
+        }
+    }
+}
