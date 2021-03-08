d35950e4f4b3d51a7fea8cc3407de926a0cadeff
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 13:02:09 +0900

    ビンゴカードの作成

---
+++ EP403/Bingo.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace EP403
+{
+  class Bingo
+  {
+    // フィールド
+    // ビンゴカードのマスのサイズ
+    public int size = 0;
+    public int[,] data;
+
+    // ビンゴカードを作成するメソッド
+    public void MakeBingoCard(int size)
+    {
+      this.size = size;
+      // sizeに合わせた二次元配列を生成
+      data = new int[size, size];
+      // 値を0で初期化
+      for(int i = 0; i < data.GetLength(0); i++)
+      {
+        for(int j = 0; j < data.GetLength(1); j++)
+        {
+          data[i,j] = 0;
+        }
+      }
+      // 乱数のインスタンス
+      Random rnd = new Random();
+      // マスの値（初期値)
+      int num = 1;
+      // マスに値を設定
+      while(num <= size * size)
+      {
+        while(true)
+        {
+          int m = rnd.Next(size);
+          int n = rnd.Next(size);
+          // マスに値に設定されていない時,マス値をセット
+          if(data[m, n] == 0)
+          {
+            data[m, n] = num;
+            // マスの値を増加
+            num ++;
+          }
+          break;
+        }
+      }
+    }
+
+    // 作成したビンゴカードを表示するメソッド
+    public void ShowCard()
+    {
+      for(int i = 0; i < data.GetLength(0); i++)
+            {
+                // 数値と縦線を表示
+                for(int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write("{0, 2:d}|", data[i, j]);
+                }
+                Console.WriteLine();
+                // 下線を表示
+                for(int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write("---");
+                }
+                Console.WriteLine();
+            }
+    }
+  }
+}
---
+++ EP403/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EP403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ビンゴクラスのインスタンス
+            Bingo bingo = new Bingo();
+            // ビンゴのサイズ入力
+            Console.Write("ビンゴのサイズ : ");
+            bingo.size = int.Parse(Console.ReadLine());
+            // ビンゴカードの作成
+            bingo.MakeBingoCard(bingo.size);
+            // カードの表示
+            bingo.ShowCard();
+        }
+    }
+}
