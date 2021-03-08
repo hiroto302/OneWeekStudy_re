b821c7b1eb36593ea81a9766ddf6c2c0291459b5
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 15:51:01 +0900

    bingoEP403の改良

---
+++ EP406/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EP406
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ビンゴクラスのインスタンス
+            Bingo bingo = new Bingo();
+            // ビンゴのサイズ入力
+            Console.Write("ビンゴのサイズ : ");
+            bingo.Size = int.Parse(Console.ReadLine());
+            // ビンゴカードの作成
+            bingo.MakeBingoCard(bingo.Size);
+            // カードの表示
+            bingo.ShowCard();
+        }
+    }
+}
