c4251b388c986b657884c70b481f85b87e8c08d0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 20:13:55 +0900

    EP406BINGOにコンストラクタ追加

---
+++ EP409/Bingo.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace EP409
+{
+  class Bingo
+  {
+    // フィールド
+    // ビンゴカードのマスのサイズ
+    private int size = 0;
+    // private化
+    private int[,] data;
+
+    // プロパティ
+    public int Size
+    {
+      set{ size = value;}
+      get{ return size;}
+    }
+
+    // コンストラクタ 引数あり
+    public Bingo(int size)
+    {
+      MakeBingoCard(size);
+    }
+    // 引数なし
+    public Bingo() : this (5)
+    {
+    }
+
+    // dataの初期化
+    private void Clear()
+    {
+      // 値を0で初期化
+      for(int i = 0; i < data.GetLength(0); i++)
+      {
+        for(int j = 0; j < data.GetLength(1); j++)
+        {
+          data[i,j] = 0;
+        }
+      }
+    }
+    // dataにに数をランダムに配置
+    private void Shuffle()
+    {
+      // 乱数のインスタンス
+      Random rnd = new Random();
+      // マスの値（初期値)
+      int num = 1;
+      // マスに値を設定
+      while(num <= size * size)
+      {
+        while(true)
+        {
+          int m = rnd.Next(size);
+          int n = rnd.Next(size);
+          // マスに値に設定されていない時,マス値をセット
+          if(data[m, n] == 0)
+          {
+            data[m, n] = num;
+            // マスの値を増加
+            num ++;
+          }
+          break;
+        }
+      }
+    }
+    // ビンゴカードを作成するメソッド
+    public void MakeBingoCard(int size)
+    {
+      this.size = size;
+      // sizeに合わせた二次元配列を生成
+      data = new int[size, size];
+      // 値を0で初期化
+      Clear();
+      // 値の配置
+      Shuffle();
+    }
+
+    // 作成したビンゴカードを表示するメソッド
+    public void ShowCard()
+    {
+      for(int i = 0; i < data.GetLength(0); i++)
+            {
+                // 数値と縦線を表示
+                for(int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write("{0, 2:d}|", data[i, j]);
+                }
+                Console.WriteLine();
+                // 下線を表示
+                for(int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write("---");
+                }
+                Console.WriteLine();
+            }
+    }
+  }
+}
---
+++ EP409/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EP409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ビンゴのサイズ
+            int size;
+            // ビンゴのサイズ入力
+            Console.Write("ビンゴのサイズ : ");
+            size = int.Parse(Console.ReadLine());
+            // ビンゴクラスのインスタンス
+            Bingo bingo = new Bingo(size);
+            // ビンゴカードの作成
+            // bingo.MakeBingoCard(bingo.Size);
+            // カードの表示
+            bingo.ShowCard();
+        }
+    }
+}
