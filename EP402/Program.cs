15e3a3b648bf1355559af0ceb195051cb4f435c5
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 12:18:14 +0900

    stringクラスの活用, エイリアスについて, ToUpper(), ToLower()

---
+++ EP402/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EP402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("英文を入力 : ");
+            string str = Console.ReadLine();
+            // 大文字で表示
+            Console.WriteLine("大文字 : {0}", str.ToUpper());
+            Console.WriteLine("小文字 : {0}", str.ToLower());
+        }
+    }
+}
