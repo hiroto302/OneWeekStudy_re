f6cca8825a3410efa372d6d5de8259547f21a79b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 15:33:06 +0900

    string クラス Lengthメソッド

---
+++ EP405/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EP405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力 : ");
+            string str = Console.ReadLine();
+            Console.WriteLine("文字数 : {0}", str.Length);
+        }
+    }
+}
