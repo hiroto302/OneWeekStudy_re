5b9d25afd0ee026c73af3012294503e0bdc5c314
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-08 11:39:42 +0900

    戻り値のあるメソッド、オーバーロード（同名のメソッドの定義）

---
+++ S_402/Calc.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace S_402
+{
+  class Calc
+  {
+    // 2つの和を求めるメソッド
+    public int Add(int a, int b)
+    {
+      return a + b;
+    }
+    // 3つ
+    public int Add(int a, int b, int c)
+    {
+      return a + b + c;
+    }
+  }
+}
---
+++ S_402/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace S_402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcクラスのインスタンス生成
+            Calc calc = new Calc();
+            int a = 1, b = 2, c = 3;
+            int ans1 = calc.Add(a, b);
+            int ans2 = calc.Add(a, b, c);
+            Console.WriteLine(ans1);
+            Console.WriteLine(ans2);
+        }
+    }
+}
