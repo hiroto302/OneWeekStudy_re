191b03d8011e8d224018bb4ad403a007ca689085
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-10 12:41:30 +0900

    HashSet
    重複なくデータ管理

---
+++ S_604/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S_604
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+            a.Add(4);
+            a.Add(3);
+            a.Add(1);
+            foreach(int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
