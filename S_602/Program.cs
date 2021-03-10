94adaeedff2d58e414a686f0c6c96d0e7c9230c2
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-10 12:06:28 +0900

    List(Reamove, ReamoveAt)

---
+++ S_602/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S_602
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = new List<string>();
+            // data追加
+            a.Add("one");
+            a.Add("two");
+            a.Add("three");
+            a.Add("two");
+            a.Add("four");
+            // data削除
+            a.Remove("two");
+            a.RemoveAt(3);
+            foreach( string s in a)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
