378a9d09faa61d5473b152f5e2fc237203567545
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-10 15:41:52 +0900

    意図的な例外処理 throw

---
+++ S_608/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace S_608
+{
+    class Program
+    {
+        static int GetNum(int i)
+        {
+            int[] nums = {300, 600, 900};
+            // 範囲外の時の処理
+            if(i > nums.Length)
+            {
+                // 例外を発生させる
+                throw new IndexOutOfRangeException();
+            }
+            return nums[i];
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int result =  GetNum(4);
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("範囲外");
+            }
+        }
+    }
+}
