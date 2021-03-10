19caa93edd2a789a2c24b36895180a25ad8bec7c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-10 22:50:10 +0900

    3つの入力された数値の和と平均

---
+++ Prob103/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prob103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力される実数値を格納するList
+            List<double> n = new List<double>();
+            // 文字列の配列
+            String[] s = {"a", "b", "c"};
+            // 実数値の入力
+            for(int i = 0; i < s.Length; i++)
+            {
+                Console.Write("{0} = ", s[i]);
+                n.Add(double.Parse(Console.ReadLine()));
+            }
+            // 結果の表示
+            Console.WriteLine("sum : " + n.Sum());
+            Console.WriteLine("avg : " + n.Average());
+        }
+    }
+}
