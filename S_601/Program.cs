749ff0f3e7b7ddc40119412a1fe8f840eda12ade
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-10 11:51:42 +0900

    コレクション, List,メソッド(Add, Insert, Count)

---
+++ S_601/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S_601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List変数
+            List<int> a = new List<int>();
+            // Add 値を0番目から順に挿入
+            a.Add(3);
+            a.Add(2);
+            a.Add(1);
+            // Insert 1番目に4を挿入 a[1] = 4 となる
+            a.Insert(1, 4);
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine("a[{0}] = {1}", i, a[i]);
+            }
+        }
+    }
+}
