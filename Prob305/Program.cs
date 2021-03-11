61dbc851a182c7f75104ad3db829ffcdfdab392a
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-11 11:08:18 +0900

    Prob306,305

---
+++ Prob305/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prob305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 生成するランダム値の個数
+            int n = 5;
+            // 生成したランダム値を格納するList
+            List<int> nums = new List<int>();
+            // ランダムクラスのインスタンス
+            Random rnd = new Random();
+            // Listに５個追加
+            while( n > 0)
+            {
+                nums.Add(rnd.Next(1, 100));
+                n--;
+            }
+            // Listの表示
+            foreach(int i in nums)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("最大値 : {0} 最小値 : {1}", nums.Max(), nums.Min());
+
+        }
+    }
+}
---
+++ Prob306/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prob306
+{
+    // 入力した文字を返すプログラム
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while文を利用した記述
+            while(true)
+            {
+                Console.Write("文字列を入力 : ");
+                string s = Console.ReadLine();
+                if(s == "")
+                {
+                    Console.WriteLine("終了");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
