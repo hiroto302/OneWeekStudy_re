96fa057e73c8736d6d51016ba06333b58067cff2
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-11 14:00:20 +0900

    HashSet, Dictionaryの利用

---
+++ Prob308/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prob308
+{
+    // 1~100の素数を表示するプログラム
+    class Program
+    {
+        // 引数の値が素数であるか
+        private static bool PrimNumberTest(int n)
+        {
+            // 引数が持つ約数の数 初期化
+            int i = 0;
+            // nを1~nの順で剰余
+            for(int j = 1; j <= n; j++)
+            {
+                if(n % j == 0)
+                {
+                    i++;
+                }
+            }
+            // 素数の判定 約数の数により判断
+            if(i > 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // 素数の表示
+            // 1~100生成
+            for(int i = 1; i <= 100; i++)
+            {
+                // 素数判定
+                if(PrimNumberTest(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Prob404/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prob404
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // key int型の月 , value 文字列の月
+            Dictionary<int , string> month = new Dictionary<int, string>();
+            // データの追加
+            month[1] = "一月";
+            month[2] = "二月";
+            month[3] = "三月";
+            month[4] = "四月";
+            try
+            {
+                // ユーザーによる入力
+                Console.Write("1~4を入力 : ");
+                int m = int.Parse(Console.ReadLine());
+                // 対応したvalueの表示
+                Console.WriteLine("{0}", month[m]);
+            }
+            catch(KeyNotFoundException)
+            {
+                Console.WriteLine("適切な値を入力せよ");
+            }
+        }
+    }
+}
